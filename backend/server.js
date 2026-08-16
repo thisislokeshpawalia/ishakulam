e2e4db8132ad1dc95f4dfb381a56eae8e6341668
thisislokeshpawalia <this.is.lokesh.pawalia@gmail.com>
2026-08-16 14:09:08 +0530

    Fix CORS issue to allow requests from ishakulam.com

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:8080'
+  origin: ['http://localhost:8080', 'http://localhost:5173', 'https://www.ishakulam.com', 'https://ishakulam.com']
 }));
 app.use(express.json());
 
